70bfc06f53996b5d58e515c5f48e8209f10c50eb
unknown <huutam.ptnk@gmail.com>
2019-02-07 03:40:03 +0200

    added profilePicture.js

---
+++ src/component/profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    state = { }
+    render() { 
+        return (  
+            <div className="profile">
+                
+            </div>
+        );
+    }
+}
+ 
+export default Profile;
---
+++ src/component/profilePicture.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProfilePicture extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div className="profileImg">
+                <img src="" alt="Profile Picture" width="42" height="42"></img>
+            </div>
+        );
+    }
+}
+ 
+export default ProfilePicture;
