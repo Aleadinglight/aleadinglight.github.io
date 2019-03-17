41b3e240a6b2628edd2a4b20f44612dfebfb17b2
Admin <huutam.ptnk@gmail.com>
2019-03-17 06:54:35 +0200

    added react-bootstrap and bootstrap

---
+++ src/component/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Nav } from 'react-bootstrap';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Nav navbar-expand-lg nav-scroll>
+            </Nav>
+        );
+    }
+}
+ 
+export default Navbar;
