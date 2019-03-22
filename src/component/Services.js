21382791c9fe0faf5a4f49cdf8c82f7536f7e2f4
Admin <huutam.ptnk@gmail.com>
2019-03-22 09:51:05 +0200

    added Services.js

---
+++ src/component/Services.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col, ProgressBar } from 'react-bootstrap';
+
+class Services extends Component {
+  state = {}
+  render() {
+    return ( 
+      <Container>
+        <Row>
+
+        </Row>
+
+        <Row>
+          
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Services;
