7a0fadb5b094f6d0fae87f8c012fa3aa12f45dd5
Admin <huutam.ptnk@gmail.com>
2019-03-26 15:24:52 +0200

    added build

---
+++ build/precache-manifest.9cf0b5f00bb8c91cd3b77c4d0034271c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb9898196bec33915fac9c0e2239065f",
+    "url": "/static/media/digitreco.cb989819.png"
+  },
+  {
+    "revision": "b904dbb6a16d63d42d14",
+    "url": "/static/css/main.9208bdd8.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d313bbbc471e901f5d654697d7a85e1",
+    "url": "/static/media/redemption.4d313bbb.jpg"
+  },
+  {
+    "revision": "dcf5d5cd194c6b687f70",
+    "url": "/static/js/2.9e1935cd.chunk.js"
+  },
+  {
+    "revision": "ff5a08d2d1a40a8eb4ecc33354c12d07",
+    "url": "/static/media/map.ff5a08d2.png"
+  },
+  {
+    "revision": "2455d2acb388c6c2a9ed589a93b51d3e",
+    "url": "/static/media/askai.2455d2ac.png"
+  },
+  {
+    "revision": "65b8ebf6da8149d5a7954d6a7df7cea1",
+    "url": "/static/media/artstyle.65b8ebf6.png"
+  },
+  {
+    "revision": "b904dbb6a16d63d42d14",
+    "url": "/static/js/main.094ac56b.chunk.js"
+  },
+  {
+    "revision": "5589a6489e8652ee6fa7e9129c065f8a",
+    "url": "/static/media/particle.5589a648.png"
+  },
+  {
+    "revision": "25ed6fe05a4aa3f2d7062703d0f5245e",
+    "url": "/static/media/aug.25ed6fe0.jpg"
+  },
+  {
+    "revision": "c01ef2f4b4d82b705814c21c834386a7",
+    "url": "/static/media/raspberry.c01ef2f4.jpg"
+  },
+  {
+    "revision": "57c3e7db4b104b4368d7a944d6e7a7fc",
+    "url": "/static/media/chart.57c3e7db.jpg"
+  },
+  {
+    "revision": "69c53ca3d3c68f786027db205afe331b",
+    "url": "/static/media/scraping.69c53ca3.png"
+  },
+  {
+    "revision": "dcf5d5cd194c6b687f70",
+    "url": "/static/css/2.9868761b.chunk.css"
+  },
+  {
+    "revision": "1b0e58693f00d7a536626f6cd5a2f7e5",
+    "url": "/index.html"
+  }
+];
