3ae6efaf2373e51761336726ba990d7a44a7f8ac
Tam Nguyen <tamnguyen@excosoft.fi>
2021-09-27 01:32:15 +0300

    Added Navbar and LandingPage section.

---
+++ src/components/mynavbar/MyNavbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import './MyNavbar.css'
+
+export default class MyNavbar extends Component {
+  render() {
+    return (
+      <div className="my-navbar-container">
+        <Navbar bg="dark" variant="dark" fixed="top">
+          <Container>
+            <Navbar.Brand href="#landing-page">Tam Nguyen</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#about-me">About</Nav.Link>
+              <Nav.Link href="#experience-container">Experience</Nav.Link>
+              <Nav.Link href="">Porfolio</Nav.Link>
+              <Nav.Link href="">Contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+    )
+  }
+}
