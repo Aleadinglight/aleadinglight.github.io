c5e993925a2d9563fc5d81a7105c2e71bab2b0e3
Tam Nguyen <tamnguyen@excosoft.fi>
2021-09-27 01:31:02 +0300

    Added About section.

---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './About.css'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div id='about-me'>
+        This is about me
+      </div>
+    )
+  }
+}
