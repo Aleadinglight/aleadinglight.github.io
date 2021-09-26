36978cb836a4ad421260fdf3927ced4a08f7fe65
Tam Nguyen <tamnguyen@excosoft.fi>
2021-09-26 19:33:45 +0300

    Added react-bootstrap and bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/landingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
+      <LandingPage />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
