f83bb967aa80cb8f335927695a15cf98ff669d0f
Tam Nguyen <tamnguyen@excosoft.fi>
2021-10-02 04:14:34 +0300

    Update About section and quote.json

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,19 +14,23 @@ export default class About extends Component {
           <Row>
             <Col md={7}>
               <p className="about-me-dialog">
-                Hello! I am Tam Nguyen, a programmer and sometimes a technical writer. I am passionate about building excellent software that benefits people. My story dates back to high school when I decided to participate in a Programming class because I was bored. It turns out to be one of the most significant decisions I have ever made because I have fallen in love with programming ever since. <br/><br/>
+                Hello! My name is Tam Nguyen, a programmer and sometimes a technical writer. I am passionate about building excellent software that benefits people. My story dates back to high school when I decided to participate in a Programming class out of boredom. It turns out to be one of the most significant decisions I have ever made because I have fallen in love with programming ever since. <br /><br />
 
-                Fast-forward to this day, I am currently a software developer with 4+ years of professional experience. I am lucky to have the opportunity to work on various kinds of projects, including <b>a small internal application</b> for start-ups, <b>web applications</b> for big customers, and currently <b>a full-scale enterprise analytics platform.</b> <br/><br/>
+                Fast-forward to this day, I am currently a software developer with 4+ years of professional experience. I am lucky to have the opportunity to work on various kinds of projects, including <b>a small internal application</b> for start-ups, <b>web applications</b> for big customers, and currently <b>a full-scale enterprise analytics platform.</b> <br /><br />
 
-                Thanks to these years in the field, I acquired more knowledge, developed an engineering mindset, and improved my time management skills. I also improved my ability to communicate with team members and clients while working through milestones and deadlines. <br/><br/>
+                Thanks to these years in the field, I acquired more knowledge, developed an engineering mindset, and improved my time management skills. I also improved my ability to communicate with team members and clients while working through milestones and deadlines. <br /><br />
 
-                Besides my profession, I am somewhat of a musician in my free time. I play piano, guitar and sometimes I do make music. I am deeply in love with music and I feel that in some way, my instruments correspond to my human feeling. 
+                Besides my profession, I am somewhat of a musician in my free time. I play piano, guitar and sometimes I do make music. I am deeply in love with music and I feel that in some way, my instruments correspond to my human feeling.
               </p>
             </Col>
 
             <Col md={5} className="my-avatar-holder">
-              <Image src={avatar} className="my-avatar"/>
-              <p className="my-avatar-description">Don't let the picture scare you, I am a clown at heart.</p>
+              <div>
+                <Image src={avatar} className="my-avatar" thumbnail />
+                <div className="my-avatar-description-container">
+                  <p className="my-avatar-description">Don't let the picture scare you, I am a clown at heart.</p>
+                </div>
+              </div>
             </Col>
           </Row>
         </Container>
