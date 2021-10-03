79149e8d82928003d70d7c96bf0018fce0905c3e
Tam Nguyen <tamnguyen@excosoft.fi>
2021-10-03 05:50:07 +0300

    Fix porfolio_data missing image link.

--- src/static/porfolio_data.js
+++ src/static/porfolio_data.js
@@ -1,23 +1,45 @@
 const porfolio_data = [
   {
     name: "Interactive Tree Graph Editor",
+    image: "digits.png",
     description: "A lightweight, customizable application for creating a custom tree graph",
     language: "React, Javascript"
   },
   {
     name: "Data Augmentation",
+    image: "augmentation.jpg",
     description: "Data augmentation technies to increase the number of dataset for Deep Learning.",
     language: "MathLab"
   },
   {
     name: "Flutter Particles",
+    image: "particles.png",
     description: "A Flutter widget for creating floating particles effects. Published on pub.dev",
     language: "Flutter, Dart"
   },
   {
     name: "Real-time digit recognition",
+    image: "digits.png",
     description: "An application in Python that recognize hand-written digit using input images from desktop's camera.",
     language: "Pytorch, python"
+  },
+  {
+    name: "Art style transfer",
+    image: "scrapping.png",
+    description: "Create a mix in style and content of two pictures using the power of Convolutional Neural Network",
+    language: "Python, Pytorch"
+  },
+  {
+    name: "Job scrapping",
+    image: "scrapping.png",
+    description: "A tool in Python to gather job data from job seeking website",
+    language: "Python"
+  },
+  {
+    name: "Sound Visualization",
+    image: "raspberry.jpg",
+    description: "Visualize sound via terminal with Raspberry Pi.",
+    language: "C++, PHP, Javascript"
   }
 ]
 
