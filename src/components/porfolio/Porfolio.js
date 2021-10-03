8ee65e67807893bc8107ffa354db2fe1755b71e7
Tam Nguyen <tamnguyen@excosoft.fi>
2021-10-03 05:49:45 +0300

    Update dynamic rendering to Porfolio section.

--- src/components/porfolio/Porfolio.js
+++ src/components/porfolio/Porfolio.js
@@ -3,32 +3,45 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import porfolioData from '../../static/porfolio_data'
 import './Porfolio.css'
 
 export default class Porfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      porfolioData: porfolioData
+    };
+  }
+
   render() {
     return (
       <Container id="porfolio" className="common-section-container">
         <h2 className="my-common-header">Porfolio</h2>
-        <p className="experience-text">This is what I have been doing so far</p>
-        <div className="card-columns">
-          <Row xs={1} md={3} className="g-4" >
-            {Array.from({ length: 6 }).map((_, idx) => (
-              <Col>
-                <Card className="porfolio-card">
-                  <Card.Img variant="top" src="holder.js/100px160" />
-                  <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                      This is a longer card with supporting text below as a natural
-                      lead-in to additional content. This content is a little bit longer.
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
+        <p className="experience-text">Some worth-nothing projects</p>
+
+        <Row lg={2} xl={3}>
+          {this.state.porfolioData.map((item) => (
+            <Col className="card-column">
+              <Card className="porfolio-card">
+                <Card.Img
+                  variant="top"
+                  src={require(`../../static/images/${item.image}`).default} />
+                <Card.Body className="my-card-body">
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>
+                    {item.description}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">
+                    {item.language}
+                  </small>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
     )
   }
