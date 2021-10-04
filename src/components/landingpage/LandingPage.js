6832618c437dd77985793a43a1e8025153e3fb57
Tam Nguyen <tamnguyen@excosoft.fi>
2021-10-04 05:48:05 +0300

    Update landing page content and styling.

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+import landingIcon from '../../static/images/astronaut.svg'
 import Button from 'react-bootstrap/Button'
 import './LandingPage.css'
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div id="landing-page">
-        Hello this is landing page.
-        <Button variant="primary">Primary</Button>{' '}
+      <div>
+        <Container fluid id="landing-page">
+          <Container className="welcome-text">
+            <h1>HELLO THERE!</h1>
+            <h4>It's nice to see someone here.</h4>
+            <Button
+              href="#about-me"
+              className="welcome-button"
+              variant="light">
+              Read More
+            </Button>
+          </Container>
+          <Image
+            className="landing-page-icon"
+            src={landingIcon}>
+          </Image>
+        </Container>
       </div>
     )
   }
