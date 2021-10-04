30be225931f42f2e4f57f6cce1ffa0f95dad9ada
Tam Nguyen <tamnguyen@excosoft.fi>
2021-10-04 05:49:02 +0300

    Update Navbar color

--- src/components/mynavbar/MyNavbar.js
+++ src/components/mynavbar/MyNavbar.js
@@ -8,14 +8,17 @@ export default class MyNavbar extends Component {
   render() {
     return (
       <div className="my-navbar-container">
-        <Navbar bg="dark" variant="dark" fixed="top">
+        <Navbar
+          className="my-navbar"
+          variant="dark"
+          fixed="top">
           <Container>
             <Navbar.Brand href="#landing-page">Tam Nguyen</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="#about-me">About</Nav.Link>
               <Nav.Link href="#experience-container">Experience</Nav.Link>
               <Nav.Link href="#porfolio">Porfolio</Nav.Link>
-              <Nav.Link href="">Contact</Nav.Link>
+              <Nav.Link href="#contact-me">Contact</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
