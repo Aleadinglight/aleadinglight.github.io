11a376f4bdfe4047a0a968508998ad356c3ab94a
Tam Nguyen <tamnguyen@excosoft.fi>
2021-11-11 08:15:40 +0200

    Updated porfolio section with animation and link to projects.

--- src/components/porfolio/Porfolio.js
+++ src/components/porfolio/Porfolio.js
@@ -5,6 +5,8 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import porfolioData from '../../static/porfolio_data'
 import './Porfolio.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default class Porfolio extends Component {
   constructor(props) {
@@ -12,36 +14,39 @@ export default class Porfolio extends Component {
     this.state = {
       porfolioData: porfolioData
     };
+    AOS.init();
   }
 
   render() {
     return (
-      <Container id="porfolio" className="common-section-container">
-        <h2 className="my-common-header">Porfolio</h2>
-        <p className="experience-text">Some worth-nothing projects</p>
+      <Container id="porfolio" className="bg-light common-section-container" fluid>
+        <Container>
+          <h2 className="my-common-header">Porfolio</h2>
+          <p className="experience-text">Some worth-noting projects</p>
 
-        <Row lg={2} xl={3}>
-          {this.state.porfolioData.map((item) => (
-            <Col key={item.name} className="card-column">
-              <Card className="porfolio-card">
-                <Card.Img
-                  variant="top"
-                  src={require(`../../static/images/${item.image}`).default} />
-                <Card.Body className="my-card-body">
-                  <Card.Title>{item.name}</Card.Title>
-                  <Card.Text>
-                    {item.description}
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">
-                    {item.language}
-                  </small>
-                </Card.Footer>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+          <Row lg={2} xl={3}>
+            {this.state.porfolioData.map((item) => (
+              <Col key={item.name} className="card-column">
+                <Card className="porfolio-card" data-aos="fade-up" data-aos-duration="500">
+                  <Card.Img
+                    variant="top"
+                    src={require(`../../static/images/${item.image}`).default} />
+                  <Card.Body className="my-card-body">
+                    <Card.Title><Card.Link target="_blank" className="my-card-title" href={item.link}>{item.name}</Card.Link></Card.Title>
+                    <Card.Text>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      <b>{item.language}</b>
+                    </small>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </Container>
     )
   }
