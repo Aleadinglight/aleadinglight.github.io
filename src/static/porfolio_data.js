256aaaadc1d8a4dc42b7ffe26ff38ed1b2cc54db
Tam Nguyen <tamnguyen@excosoft.fi>
2021-11-11 08:16:08 +0200

    Update resources.

--- src/static/porfolio_data.js
+++ src/static/porfolio_data.js
@@ -1,45 +1,52 @@
 const porfolio_data = [
   {
-    name: "Interactive Tree Graph Editor",
+    name: "Task Tracking",
     image: "digits.png",
-    description: "A lightweight, customizable application for creating a custom tree graph",
+    link: "https://github.com/Aleadinglight/Task-Tracking-Web",
+    description: "A light weight, customizable task tracking web application tailored to my needs.",
     language: "React, Javascript"
   },
   {
     name: "Data Augmentation",
     image: "augmentation.jpg",
-    description: "Data augmentation technies to increase the number of dataset for Deep Learning.",
+    link: "https://github.com/Aleadinglight/Data-Augmentation",
+    description: "Data Augmentation Algorithms to increase the number of datasets for Deep Learning and Machine Learning, demonstrated in MathLab.",
     language: "MathLab"
   },
   {
     name: "Flutter Particles",
     image: "particles.png",
-    description: "A Flutter widget for creating floating particles effects. Published on pub.dev",
+    link: "https://github.com/Aleadinglight/Flutter-Particles",
+    description: "A Flutter widget for creating floating particles effects. Published on pub.dev as an open-source project.",
     language: "Flutter, Dart"
   },
   {
-    name: "Real-time digit recognition",
-    image: "digits.png",
-    description: "An application in Python that recognize hand-written digit using input images from desktop's camera.",
-    language: "Pytorch, python"
-  },
-  {
     name: "Art style transfer",
-    image: "scrapping.png",
+    image: "art_transfer.png",
+    link: "https://github.com/Aleadinglight/Pytorch-VGG-19",
     description: "Create a mix in style and content of two pictures using the power of Convolutional Neural Network",
     language: "Python, Pytorch"
   },
   {
     name: "Job scrapping",
     image: "scrapping.png",
-    description: "A tool in Python to gather job data from job seeking website",
+    link: "https://github.com/Aleadinglight/Job-Description-Data-Scraping",
+    description: "A tool in Python to gather job data from job-seeking websites. This was made for some friends of mine.",
     language: "Python"
   },
   {
     name: "Sound Visualization",
-    image: "raspberry.jpg",
-    description: "Visualize sound via terminal with Raspberry Pi.",
+    image: "sound_visualize.png",
+    link: "https://github.com/Aleadinglight/Rasp_AppDev",
+    description: "Visualize sound by displaying the data as a time-series graph via the Linux terminal. Built for Raspberry Pi 3.",
     language: "C++, PHP, Javascript"
+  },
+  {
+    name: "Real-time digit recognition",
+    image: "digits.png",
+    link: "https://github.com/Aleadinglight/Real-time-handwritten-digits-recognition",
+    description: "An application in Python that recognizes hand-written digit using input images from a desktop's camera.",
+    language: "Pytorch, python"
   }
 ]
 
