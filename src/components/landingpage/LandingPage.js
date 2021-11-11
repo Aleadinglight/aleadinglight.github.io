486ee455d30d00c4df78ecc97df55d4b5a5da46d
Tam Nguyen <tamnguyen@excosoft.fi>
2021-11-11 06:20:45 +0200

    Make landing page content responsive.

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 import landingIcon from '../../static/images/astronaut.png'
 import Button from 'react-bootstrap/Button'
@@ -10,20 +12,26 @@ export default class LandingPage extends Component {
     return (
       <div>
         <Container fluid id="landing-page">
-          <Container className="welcome-text">
-            <h1>HELLO THERE!</h1>
-            <h4>It's nice to see someone here.</h4>
-            <Button
-              href="#about-me"
-              className="welcome-button"
-              variant="light">
-              Read More
-            </Button>
-          </Container>
-          <Image
-            className="landing-page-icon"
-            src={landingIcon}>
-          </Image>
+          <Row className="landing-page-content">
+            <Col className="landing-page-text-container">
+              <Container className="welcome-text">
+                <h1>HELLO THERE!</h1>
+                <h4>It's nice to see someone here.</h4>
+                <Button
+                  href="#about-me"
+                  className="welcome-button"
+                  variant="light">
+                  Read More
+                </Button>
+              </Container>
+            </Col>
+            <Col className="landing-page-icon-container">
+              <Image
+                className="landing-page-icon"
+                src={landingIcon}>
+              </Image>
+            </Col>
+          </Row>
         </Container>
       </div>
     )
